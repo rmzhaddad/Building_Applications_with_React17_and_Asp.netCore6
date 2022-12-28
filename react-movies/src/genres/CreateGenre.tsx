9c098ccdf22fb278589adec15cfc0387d9797f26
rmzhaddad <rmzhaddad1@gmail.com>
2022-12-27 21:12:05 -0500

    whatever

--- react-movies/src/genres/CreateGenre.tsx
+++ react-movies/src/genres/CreateGenre.tsx
@@ -1,20 +1,38 @@
-import { useHistory } from "react-router-dom";
+import { Field, Form, Formik } from "formik";
+import React from "react";
+import { Link } from "react-router-dom";
 import Button from "../utils/Button";
 
 
 export default function CreateGenre(){
-    const history=useHistory();
+
     return(
         <>
-        <h3>
-            Create Genre
-        </h3>
-        <Button onClick={()=>{
-            //... saving in the database
-            history.push('/genres');
+            <h3>Create Genre</h3>
 
-        }}>Save Changes
-        </Button>
+            <Formik initialValues={{
+                name:''
+
+            }}
+
+            onSubmit={value=>{
+                //when the form is submitted
+                console.log(value);
+            }}
+            >
+                <Form>
+                    <div className="form-group">
+                        <label htmlFor="name">Name</label>
+                        <Field name="name" id="name" className="form-control"/>
+                    </div>
+
+                    <Button type='submit' >Save Changes</Button>
+                    <Link className="btn btn-secondary" to = "/genres">Cancel</Link>
+                
+                
+                </Form>
+            </Formik>
+
     
         </>
     )
