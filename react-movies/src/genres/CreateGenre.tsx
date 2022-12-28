eb0dd2a19486dcac4d8bef913a1bdc520770f261
rmzhaddad <rmzhaddad1@gmail.com>
2022-12-27 21:52:06 -0500

    First Form

--- react-movies/src/genres/CreateGenre.tsx
+++ react-movies/src/genres/CreateGenre.tsx
@@ -1,8 +1,9 @@
-import { Field, Form, Formik } from "formik";
+import { ErrorMessage, Field, Form, Formik } from "formik";
 import React from "react";
 import { Link } from "react-router-dom";
 import Button from "../utils/Button";
-
+import * as Yup from 'yup';
+import TextField from "../Forms/TextField";
 
 export default function CreateGenre(){
 
@@ -19,12 +20,14 @@ export default function CreateGenre(){
                 //when the form is submitted
                 console.log(value);
             }}
+            validationSchema={
+                Yup.object({
+                    name:Yup.string().required('this field is required')
+                })
+            }
             >
                 <Form>
-                    <div className="form-group">
-                        <label htmlFor="name">Name</label>
-                        <Field name="name" id="name" className="form-control"/>
-                    </div>
+                    <TextField field="name" displayName="Name"/>
 
                     <Button type='submit' >Save Changes</Button>
                     <Link className="btn btn-secondary" to = "/genres">Cancel</Link>
